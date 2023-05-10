dfa8bdfba4d064dc65a4634980f35702615152b2
vulebaolong <vulebaolong@gmail.com>
2023-05-10 20:52:13 +0700

    181. Setting up the Project Structure

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRouters');
+const viewRouter = require('./routes/viewRouters');
 
 const app = express();
 
@@ -81,25 +82,7 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'Vũng tàu',
-    user: 'vulebaolong'
-  });
-});
-
-app.get('/overview', (req, res) => {
-  res.status(200).render('overview', {
-    title: 'All Tours'
-  });
-});
-
-app.get('/tour', (req, res) => {
-  res.status(200).render('tour', {
-    title: 'một tour'
-  });
-});
-
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
