a010ffbe45af5f5fdb2fabf5d2eb8aa8ad45c8c5
vulebaolong <vulebaolong@gmail.com>
2023-05-10 19:37:09 +0700

    177. First Steps with Pug

--- app.js
+++ app.js
@@ -82,7 +82,10 @@ app.use((req, res, next) => {
 
 // 3) ROUTES
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'Vũng tàu',
+    user: 'vulebaolong'
+  });
 });
 
 app.use('/api/v1/tours', tourRouter);
