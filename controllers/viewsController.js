196207c307eb944d61b66e6053dc439a0b57bc5e
vulebaolong <vulebaolong@gmail.com>
2023-05-10 22:05:58 +0700

    183. Building the Tour Overview - Part 2

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,16 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const { catchAsync } = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1) get tour data từ collection
+  const tours = await Tour.find();
+
+  // 2) xây dựng template
   res.status(200).render('overview', {
-    title: 'All Tours'
+    title: 'All Tours',
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
