426980a5121a6c0eaaae2a733bfec608015d7643
vulebaolong <vulebaolong@gmail.com>
2023-05-18 14:25:02 +0700

    202. Resizing Images

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,21 @@
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('./../models/userModel');
 const { catchAsync } = require('./../utils/catchAsync');
 const AppError = require('./../utils/apiError');
 const factory = require('./handleFactory');
 
-const multerStorage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'public/img/users');
-  },
-  filename: function(req, file, cb) {
-    // user-345345345-234234.jpg
-    const ext = file.mimetype.split('/')[1];
-    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
-  }
-});
+// const multerStorage = multer.diskStorage({
+//   destination: function(req, file, cb) {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: function(req, file, cb) {
+//     // user-345345345-234234.jpg
+//     const ext = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+//   }
+// });
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = function(req, file, cb) {
   if (file.mimetype.startsWith('image')) {
@@ -35,6 +37,19 @@ const upload = multer({
 });
 
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = function(req, res, next) {
+  if (!req.file) {
+    return next();
+  }
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+  sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+  next();
+};
 
 // lọc ra các key allowedFields và trả về obj mới với key được lọc và value
 const filterObj = (obj, ...allowedFields) => {
