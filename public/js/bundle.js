7ee61997655c775cda6044ca49128bf45444b7a0
vulebaolong <vulebaolong@gmail.com>
2023-05-18 23:10:28 +0700

    206. Building a Complex Email Handler

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -12421,7 +12421,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54823" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59195" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- utils/email.js
+++ utils/email.js
@@ -1,37 +1,92 @@
 //https://mailtrap.io/
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-  // 1) tạo transporter
-  //   const transporter = nodemailer.createTransport({
-  //     // service: 'Gmail',
-  //     host: process.env.EMAIL_HOST,
-  //     port: process.env.EMAIL_PORT,
-  //     auth: {
-  //       user: process.env.EMAIL_USERNAME,
-  //       password: process.env.EMAIL_PASSWORD
-  //     }
-  //   });
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Vũ Lê Bảo Long <${process.env.EMAIL_FORM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // Send grid
+      return 1;
     }
-  });
 
-  // 2) thiết lập tùy chọn options
-  const mailOptions = {
-    from: 'Vũ Lê Bảo Long <vulebaolong1@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-    // html:
-  };
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
 
-  // 3) gửi email
-  await transporter.sendMail(mailOptions);
+  async send(template, subject) {
+    // 1) Render HTML từ một pug
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject
+      }
+    );
+
+    // 2) thiết lập các tùy chọn email
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html)
+    };
+
+    //3 tạo transporter và gửi email
+    const transporter = this.newTransport();
+    await transporter.sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Chào mừng đến với Natours Family');
+  }
 };
 
-module.exports = sendEmail;
+// const sendEmail = async options => {
+//   // 1) tạo transporter
+//     const transporter = nodemailer.createTransport({
+//       // service: 'Gmail',
+//       host: process.env.EMAIL_HOST,
+//       port: process.env.EMAIL_PORT,
+//       auth: {
+//         user: process.env.EMAIL_USERNAME,
+//         password: process.env.EMAIL_PASSWORD
+//       }
+//     });
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.EMAIL_HOST,
+//     port: process.env.EMAIL_PORT,
+//     auth: {
+//       user: process.env.EMAIL_USERNAME,
+//       pass: process.env.EMAIL_PASSWORD
+//     }
+//   });
+
+//   // 2) thiết lập tùy chọn options
+//   const mailOptions = {
+//     from: `Vũ Lê Bảo Long <${process.env.EMAIL_FORM}>`,
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message
+//     // html:
+//   };
+//   // 3) gửi email
+//   await transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendEmail;
